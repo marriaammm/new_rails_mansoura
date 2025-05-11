b61caed1d0e77907e3d566a6a362d661937be08f
marriaammm <mariamz@std.mans.edu.eg>
2025-05-11 17:38:18 +0300

    add address column to users table with non-null constraint and default value

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_11_143450) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_11_143713) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "content"
@@ -25,5 +25,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_11_143450) do
     t.string "phone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address", default: "", null: false
   end
 end
